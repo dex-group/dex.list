446261362b8070a5a3b9e981fcd13806969d1d83
dingyi <dingyimac@gmail.com>
2020-04-16 23:01:16 +0800

    2.2.37
    update plausible to custom domain

--- src/html.js
+++ src/html.js
@@ -12,7 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
-        <script async defer data-domain="dexlist.page" src="https://plausible.io/js/plausible.js"></script>
+        <script async defer data-domain="dexlist.page" src="https://stats.dexlist.page/js/index.js"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
