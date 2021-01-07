ceb30bf399db24835f9615bc754eabeab7c54c10
dingyi <dingyimac@gmail.com>
2021-01-07 15:05:07 +0800

    2.3.19
    - add data
    - add wwads code
    - update deps
    - fix emotion styled

--- src/html.js
+++ src/html.js
@@ -14,6 +14,16 @@ export default function HTML(props) {
         <link rel="canonical" href="https://dexlist.page/"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         <script async defer data-domain="dexlist.page" src="https://stats.dexlist.page/js/index.js"></script>
+        <!-- 万维广告联盟代码 - wwads.cn -->
+        <script type="text/javascript">
+          (function(){
+          var _sr = document.createElement('script');
+          _sr.type = 'text/javascript';
+          _sr.async = true;
+          _sr.src = 'https://wwads.cn/code/install';
+          (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(_sr);
+          })();
+        </script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
