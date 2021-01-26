fc55995418da877f2beff09035c7eb345c776d65
dingyi <dingyimac@gmail.com>
2021-01-26 16:51:59 +0800

    2.3.33
    update netlify.toml

--- src/html.js
+++ src/html.js
@@ -11,7 +11,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="canonical" href="https://dexlist.page/" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         <script async defer data-domain="dexlist.page" src="https://stats.dexlist.page/js/index.js"></script>
         <script type="text/javascript" src="https://wwads.cn/js/ads.js" async></script>
